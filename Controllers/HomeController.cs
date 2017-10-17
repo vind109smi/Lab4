32c9e6d18c87a0594b3547d910ba3b2cec7cd279
Sheikh Mahmud <mahmuds@go.stockton.edu>
2017-10-17 09:51:59 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "MVCLab4";
+            // Create a DateTime object to instantiate
+            DateTime today = DateTime.Now;
+
+            // Declare currentHour variable and check to see if time fits
+            int presentTime = today.Hour;
+            if (presentTime < 12)
+            {
+                ViewData["Greeting"] = "Good Morning!";
+            }
+            else if (presentTime < 18)
+            {
+                ViewData["Greeting"] = "Good Afternoon!";
+            }
+            else
+            {
+                ViewData["Greeting"] = "Good Evening";
+            }
+
+            //Get the remaining days using DateTime object
+            int remainingDays = 0;
+            bool isLeap = DateTime.IsLeapYear(today.Year);
+            if (isLeap)
+            {
+                remainingDays = 366 - today.DayOfYear;
+            }
+            else
+            {
+                remainingDays = 365 - today.DayOfYear;
+            }
+            ViewData["Time"] = today.ToShortTimeString();
+            ViewData["Date"] = today.ToShortDateString();
+            ViewData["Days Left"] = remainingDays;
+            ViewData["Year"] = today.Year;
+
+            return View();
+
+        }
+        public IActionResult Items(int id)
+        {
+            ViewData["ID"] = id;
+            // This is the language listing stored in an array.
+            string[] languages = { "C#","HTML5","JavaScript", "Python", "C++", "Java" };
+            return View(languages);
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelsandForms.Models
+{
+    public class Person
+    {
+        public string Name;
+        public string BirthDate;
+        public int Age;
+    }
+}
